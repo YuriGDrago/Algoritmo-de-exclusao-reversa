581fcfc8f10bd93873f2ffb310072dd27c223d29
Yuri Gomes <yuriazevedo@aluno.uespi.br>
2023-09-15 16:37:55 -0300

    exemplo2

--- Exemplo2.py
+++ Exemplo2.py
@@ -80,23 +80,19 @@ class Graph:
 if __name__ == "__main__":
 
 	# crie o gráfico fornecido na figura acima
-	V = 9
+	V = 6
 	g = Graph(V)
 
 	# fazendo o gráfico mostrado acima e dale
-	g.addEdge(0, 1, 6)
-	g.addEdge(0, 7, 9)
-	g.addEdge(1, 2, 5)
-	g.addEdge(1, 7, 8)
-	g.addEdge(2, 3, 10)
-	g.addEdge(2, 8, 3)
-	g.addEdge(2, 5, 7)
-	g.addEdge(3, 4, 4)
-	g.addEdge(3, 5, 12)
-	g.addEdge(4, 5, 11)
-	g.addEdge(5, 6, 8)
-	g.addEdge(6, 7, 2)
-	g.addEdge(6, 8, 5)
-	g.addEdge(7, 8, 1)
+	g.addEdge(0, 1, 5)
+	g.addEdge(0, 2, 8)
+	g.addEdge(0, 3, 6)
+	g.addEdge(1, 2, 7)
+	g.addEdge(1, 4, 9)
+	g.addEdge(2, 3, 4)
+	g.addEdge(2, 4, 5)
+	g.addEdge(3, 5, 3)
+	g.addEdge(4, 5, 6)
+	
 
 	g.reverseDeleteMST()
